b8532b5b86302ba8da53df55aafed287c822dd37
gnicolasyanez <g.nicolas.yanez@gmail.com>
2021-12-21 01:02:43 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './component/Menu';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" onClick={ ()=>console.log() }>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         FUTURO E-COMMERCE YAÑEZ
         </p>
+
+        <Menu />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
+    
   );
 }
 
